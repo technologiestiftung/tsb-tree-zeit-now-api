22fb941c2a2804372a3d526795c2a4e8b8f71152
fdnklg <fabian.dinklage@me.com>
2020-01-11 09:29:14 +0200

    basic request added

--- api/get-tree.ts
+++ api/get-tree.ts
@@ -1,7 +1,11 @@
-import { IncomingMessage, ServerResponse } from "http";
 import { json, send } from "micro";
 import { Pool, PoolConfig } from "pg";
 import { NowRequest, NowResponse } from "@now/node";
+
+// http://apiurl.com/api/get-tree?id=idhere
+
+const test_id = "_zxffxctnc";
+
 const pgconfig: PoolConfig = {
   user: process.env.user!,
   database: process.env.database!,
@@ -10,14 +14,13 @@ const pgconfig: PoolConfig = {
   host: process.env.host!
 };
 const pool = new Pool(pgconfig);
+
 export default async (req: NowRequest, res: NowResponse) => {
   try {
     const data = await json(req);
-    console.info(data);
-    const result = await pool.query("SELECT NOW()");
-    // await pool.end();
-
-    send(res, 200, { value: result });
+    const { id } = req.query;
+    const result = await pool.query(`SELECT * FROM trees WHERE trees.id = $1`, [test_id]);
+    send(res, 200, result.rows[0] });
   } catch (error) {
     console.error(error);
     return send(res, 400);
